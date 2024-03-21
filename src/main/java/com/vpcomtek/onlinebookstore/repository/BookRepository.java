2fe6c21d5b4c5ee477c7d2c537e1c35137b62982
Victor Rudkovsky <vpcomtek@gmail.com>
2024-03-21 15:51:11 +0200

    Add Liquibase support

--- src/main/java/com/vpcomtek/onlinebookstore/repository/BookRepository.java
+++ src/main/java/com/vpcomtek/onlinebookstore/repository/BookRepository.java
@@ -3,8 +3,5 @@ package com.vpcomtek.onlinebookstore.repository;
 import com.vpcomtek.onlinebookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
