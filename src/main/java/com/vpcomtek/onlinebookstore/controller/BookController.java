ca05e7397086dd8fb6f051d71a3127479769b7d5
Victor Rudkovsky <vpcomtek@gmail.com>
2024-03-22 02:26:17 +0200

    Implementув soft delete concept for the Book entity

--- src/main/java/com/vpcomtek/onlinebookstore/controller/BookController.java
+++ src/main/java/com/vpcomtek/onlinebookstore/controller/BookController.java
@@ -5,11 +5,14 @@ import com.vpcomtek.onlinebookstore.dto.CreateBookRequestDto;
 import com.vpcomtek.onlinebookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -31,5 +34,11 @@ public class BookController {
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 }
--- src/main/java/com/vpcomtek/onlinebookstore/model/Book.java
+++ src/main/java/com/vpcomtek/onlinebookstore/model/Book.java
@@ -6,24 +6,40 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
+
+import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE book SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+//@Table(name = "book")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private String author;
+
     @Column(nullable = false, unique = true)
     private String isbn;
+
     @Column(nullable = false)
     private BigDecimal price;
+
     @Column
     private String description;
+
     @Column(name = "cover_image")
     private String coverImage;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
