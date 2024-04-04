52e9a83919d378b6e9353c1dbdee543c5da18a61
Victor Rudkovsky <vpcomtek@gmail.com>
2024-04-04 17:21:58 +0300

    Spring data jpa (#2)
    * Add JpaRepository Interface
    
    * Add Liquibase support
    
    * Change ddl-auto property value to validate
    
    * Implementув soft delete concept for the Book entity
    
    * Added update specific book endpoint
    
    * Added update specific book endpoint
    
    * made changes after codereview
    
    * made changes after codereview
    
    * SQLRestriction annotation fixed

--- src/main/java/com/vpcomtek/onlinebookstore/model/Book.java
+++ src/main/java/com/vpcomtek/onlinebookstore/model/Book.java
@@ -8,13 +8,12 @@ import jakarta.persistence.Id;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Data
-@SQLDelete(sql = "UPDATE book SET is_deleted = true WHERE id=?")
-@Where(clause = "is_deleted=false")
-//@Table(name = "book")
+@SQLDelete(sql = "UPDATE book SET is_deleted = TRUE WHERE id = ?")
+@SQLRestriction("is_deleted = FALSE")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
