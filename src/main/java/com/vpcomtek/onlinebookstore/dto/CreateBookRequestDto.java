544d4c6e885ceaa01bbd2779aa891a93826885f9
Victor Rudkovsky <vpcomtek@gmail.com>
2024-04-25 14:44:52 +0300

    Global exception handler (#7)
    * Add JpaRepository Interface
    
    * Add Liquibase support
    
    * Change ddl-auto property value to validate
    
    * Implementув soft delete concept for the Book entity
    
    * Added update specific book endpoint
    
    * Added update specific book endpoint
    
    * Spring data jpa (#2)
    
    * Add JpaRepository Interface
    
    * Add Liquibase support
    
    * Change ddl-auto property value to validate
    
    * Implementув soft delete concept for the Book entity
    
    * Added update specific book endpoint
    
    * Added update specific book endpoint
    
    * made changes after codereview
    
    * made changes after codereview
    
    * SQLRestriction annotation fixed
    
    * made changes after codereview
    
    * made changes after codereview
    
    * SQLRestriction annotation fixed
    
    * GlobalExceptionHandler and Data Validation
    
    * validation annotations was fixed
    
    * checkstyle fixed

--- src/main/java/com/vpcomtek/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/vpcomtek/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,15 +1,27 @@
 package com.vpcomtek.onlinebookstore.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateBookRequestDto {
+    @NotBlank
+    @Size(max = 255, message = "Text limit 255 symbols")
     private String title;
+    @NotBlank
+    @Size(max = 255, message = "Text limit 255 symbols")
     private String author;
+    @ISBN(type = ISBN.Type.ANY)
+    @NotBlank
     private String isbn;
+    @NotBlank
+    @Positive
     private BigDecimal price;
     private String description;
     private String coverImage;
