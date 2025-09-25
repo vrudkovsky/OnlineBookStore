ea9a4b63588cc1888e1c1134085c966216b52586
Victor Rudkovsky <vpcomtek@gmail.com>
2025-09-25 23:11:48 +0300

    fix: PR comments

--- src/main/java/com/vpcomtek/onlinebookstore/model/User.java
+++ src/main/java/com/vpcomtek/onlinebookstore/model/User.java
@@ -9,11 +9,13 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Getter
 @Setter
 @Entity
 @SQLDelete(sql = "UPDATE users SET is_deleted = TRUE WHERE id = ?")
+@SQLRestriction("is_deleted = FALSE")
 @Table(name = "users")
 public class User {
 
